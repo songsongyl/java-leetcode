a6e3e596ef694f06999c3054b58128d798a0628a
songsongyl <2444616264@qq.com>
2024-05-08 20:37:31 +0800

    init

--- src/main/java/com/example01/TwoSum.java
+++ src/main/java/com/example01/TwoSum.java
@@ -8,21 +8,32 @@ public class TwoSum {
     public static void main(String[] args){
         Map<Integer,Integer> record = new HashMap<>();
         Scanner sc = new Scanner(System.in);
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        System.out.println("输入数组长度为：" + n);
-        int[] nums = new int[n];
-        for (int i = 0; i < n; i++){
-            nums[i]=scanner.nextInt();
+//        Scanner scanner = new Scanner(System.in);
+//        int n = scanner.nextInt();
+//        System.out.println("输入数组长度为：" + n);
+//        int[] nums = new int[n];
+//        for (int i = 0; i < n; i++){
+//            nums[i]=scanner.nextInt();
+//        }
+        System.out.println("请输入几个数并用逗号隔开：");
+        Scanner sc1 = new Scanner(System.in);
+        String str = sc1.next().toString();
+        String[] arr  = str.split(",");
+        int[] nums = new int[arr.length];
+        for(int j = 0; j<nums.length;j++) {
+            nums[j] = Integer.parseInt(arr[j]);
+            System.out.println(nums[j]+" ");
         }
 
         int a = sc.nextInt();
         System.out.println("输入目标为：" + a);
         sc.close();
-        for (int i = 0 ; i < nums.length ;i ++){
+        record.put(nums[0],0);
+        for (int i = 1 ; i < nums.length ;i ++){
             int num = a - nums[i];
             if(record.containsValue(num)){
-                System.out.println(record.get(num) + i);
+                //return new int[]{i,record.get(num)};
+                System.out.println(record.get(num) + " " + i);
             }
             record.put(nums[i],i);
         }
